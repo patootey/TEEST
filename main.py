517d3529bb4cd160598f7fe8321f96de5e65aaae
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2023-04-18 10:49:20 +0200

    added Graph class importet matplotlib.pyplot

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-print("hello")
-masse = 1000
-print("test")
-WUGUAGU = 2
-print("ja, hei")
-print("mathilde er mango")
+import matplotlib.pyplot as pyplot
 
 
 class Test:
     def __init__(self):
         self.masse = 0
         self.areal = 0
+
+
+class Graph:
+    def draw(self, x, y, title, color):
+        plot.title(str(title))
+        plot.plot(x, y, color=str(color))
+        plot.grid()
+        plot.show()
